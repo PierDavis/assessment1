c54a9cf274b73ddbef8ba8425889a4cedf51bd94
Pier Davis <pieramelia2@gmail.com>
2021-10-15 15:06:40 -0400

    origin

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/let Step1 = {name: "Analysis Of Requirements", definition: "Text here"}
+// console.log(Step1.definition)
+let sldc = [{name: "Analysis Of Requirements", definition: "Text here"},
+{name: "Design", definition: "Text here2"}]
+//console.log(sldc)
+console.log("definition2", sldc[1].definition)
+
+let Step1AnalysisOfRequirements = ("Definition text here")
