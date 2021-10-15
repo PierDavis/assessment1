be986be43e158cc9fc09e56e7eb7755b0e6b66e3
Pier Davis <pieramelia2@gmail.com>
2021-10-15 16:09:14 -0400

    variables edits

--- variables.js
+++ variables.js
@@ -2,16 +2,21 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+let myName = ("Pier Amelia Davis")
+console.log(myName)
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+let faveNum = ("2")
+console.log(faveNum)
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+let lovesCode = (false)
+console.log(lovesCode)
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,10 +26,15 @@
 */
 
 //CODE HERE
+if (faveNum === 13) 
+console.log('lucky 13!')
+else (faveNum !==13)
+console.log("not lucky 13")
 
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
-*/
 
 //CODE HERE
+*/ for (i = faveNum; i < 5;)
+//console.log(i)
