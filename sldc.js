53bfeb1b39af9cb4ac5cb48ecf3d118df0bcbe54
Pier Davis <pieramelia2@gmail.com>
2021-10-15 15:51:40 -0400

    sldc edits

--- sldc.js
+++ sldc.js
@@ -5,22 +5,28 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Analysis of Requirements
-// Design
-// Implementation
-// Maintain
-// Planning
-// Testing & Integration
-
+//Reordered:
+//Planning
+//Analysis of Requirements
+//Design
+//Implementation
+//Testing & Integration
+//Maintenance
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
-*/let Step1 = {name: "Analysis Of Requirements", definition: "Text here"}
-// console.log(Step1.definition)
-let sldc = [{name: "Analysis Of Requirements", definition: "Text here"},
-{name: "Design", definition: "Text here2"}]
-//console.log(sldc)
-console.log("definition2", sldc[1].definition)
 
-let Step1AnalysisOfRequirements = ("Definition text here")
+ *///let Step1 = {name: "Analysis Of Requirements", definition: "Text here"}
+// // console.log(Step1.definition)
+// let sldc = [{name: "Analysis Of Requirements", definition: "Text here"},
+// {name: "Design", definition: "Text here2"}]
+// //console.log(sldc)
+// console.log("definition2", sldc[1].definition)
+
+let Step1Planning = ("Identifying the scope and purpose of the project, along with project and team needs")
+let Step2AnalysisOfRequirements = ("Define requirements and resources needed to create project")
+let Step3Design = ("Documents process and creates solutions for how the software will work")
+let Step4Implementation = ("Engineers take the design and bring it to life, writing the program")
+let Step5TestingAndIntegration = ("Stage of testing before software is made public-facing, creates a better program")
+let Step6Maintenance = ("Frequent debugging, and improvements made to optomize program")
