f687efe9fb38d71ac636e4dbf96c6d6ee835e8f1
Pier Davis <pieramelia2@gmail.com>
2021-10-31 19:44:45 -0400

    password

--- password.js
+++ password.js
@@ -7,10 +7,13 @@ const reader = readline.createInterface({
 
 
 
-reader.question("Please enter a password.", function(input){
+reader.question("Enter a password", function(input){
+console.log(input)
+const userPassword = input;
+//const userPasswordPier = 'starship';
+console.log('password is', userPassword)
+//console.log('Pier password is', userPasswordPier)
 
-
-    
     	// This line closes the connection to the command line interface.
 	reader.close()
 
